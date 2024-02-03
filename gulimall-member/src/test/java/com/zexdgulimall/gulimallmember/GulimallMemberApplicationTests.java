8367bcff862aca905990e44ce38e6aea41d0aa27
2exd <416312263@qq.com>
2024-02-03 15:22:27 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/zexdgulimall/gulimallmember/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zexdgulimall.gulimallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
